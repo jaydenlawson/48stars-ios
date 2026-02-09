1db16124fdb6c3bf786d74506c8627c7f965202a
Jayden Lawson <git@thermosis.com>
2026-02-09 17:47:20 +1000

    Updates

---
+++ Four Point Eight Stars/Four Point Eight Stars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Four Point Eight Stars
+//
+//  Created by Results on 9/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Four Point Eight Stars/Four Point Eight Stars/Four_Point_Eight_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Four_Point_Eight_StarsApp.swift
+//  Four Point Eight Stars
+//
+//  Created by Results on 9/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Four_Point_Eight_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
